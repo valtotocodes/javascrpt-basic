c246e4c74c81f880c6c1109b9bf548453dc9fd30
valtotocodes <valentine.mueni@s.karu.ac.ke>
2024-05-29 12:14:27 -0700

    javascript

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function average(num1, num2, num3) {
+	var result = (num1, num2, num3) / 3;
+	return result;
+}
+var Myaverage = average(56, 56, 89);
+alert("allow me to locate you");
+document.write(Myaverage);
+
+console.log('hello Valentine');
+let name = 'Grace';
+console.log(name);
+let person = {
+	name :'josh',
+	age : 12
+};
+let selectedColors = [
+	'red', 'blue'
+];
+console.log(selectedColors[1]);
+function greet(name){
+console.log('helo' + name);
+}
+greet('john');
+function sum(number){
+	return number + number;
+}
+console.log(sum(2));
+
+function changetext(){
+	var list = document.getElementsByClassName("list");
+	for(var i = 0;i < list.length; i++){
+	list[i].innerHTML = "chase the bag";
+	list[i].style.color = "blue";}
+}
+function changethefirstletter(){
+	var demo = document.getElementById("demo");
+	demo.innerHTML = "H";
+	demo.style.color = "red";
+}
+
+ 
